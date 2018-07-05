861a194d8c66d7db7d7a7c70e1be9b6fa4111736
kedarivijaya <kedarivijaya39@gamil.com>
2018-07-04 17:58:20 -0700

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function loadJSON(file,callback){
+  var xhr=new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange=function(){
+    if(xhr.readyState===4 && xhr.status=="200"){
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
+loadJSON('json/data.json',function(text){
+  var data=JSON.parse(text);
+  console.log(data);
+  profileData(data.profiles);
+})
+var mainDiv=document.querySelector(".main");
+function profileData(pro){
+
+  for(var i=0; i<pro.length;i++){
+    var subDiv=document.createElement("div");
+    subDiv.classList.add("subdiv");
+    mainDiv.appendChild(subDiv);
+    var img=document.createElement("img");
+    img.src="images/images.jpg";
+    img.alt="image";
+    subDiv.appendChild(img);
+    var name=document.createElement("h2");
+    name.textContent=pro[i].name;
+    var label=document.createElement("h3");
+    label.textContent=pro[i].label;
+    var company=document.createElement("h3");
+    company.textContent=pro[i].company;
+    var link=document.createElement("a");
+    link.href="resume.html?id="+pro[i].id;
+    link.textContent="View Profile";
+    subDiv.appendChild(name);
+    subDiv.appendChild(label);
+    subDiv.appendChild(company);
+    var link=document.createElement("a");
+    link.href="resume.html?id="+pro[i].id;
+    link.textContent="View Profile";
+    subDiv.appendChild(link);
+
+  }
+}
